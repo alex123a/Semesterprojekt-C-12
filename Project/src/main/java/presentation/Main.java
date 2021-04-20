3c4028b7c6067d598d1db0c7af74f35eae88a0f5
Thomas <Raithdk@gmail.com>
2021-04-20 12:56:25 +0200

    moved files

--- Project/src/main/java/presentation/Main.java
+++ Project/src/main/java/presentation/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/layout/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
