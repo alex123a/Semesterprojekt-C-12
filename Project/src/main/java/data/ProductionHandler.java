88c6ef511c3fb35e9bb38c1cfd09b0941fb8db33
magnuskjaer <magnus.kjaer310101@gmail.com>
2021-05-17 11:43:05 +0200

    Made postgres work .. maybe

--- Project/src/main/java/data/ProductionHandler.java
+++ Project/src/main/java/data/ProductionHandler.java
@@ -8,10 +8,40 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 
 class ProductionHandler {
     private File file;
+
+    private static Connection connection = null;
+
+    // Singleton
+    private final static ProductionHandler prHandler = new ProductionHandler();
+
+    public ProductionHandler() {
+        try {
+            connection = getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Establish a connection to the Database
+     * @return Connection
+     */
+    private Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new org.postgresql.Driver());
+        return DriverManager.getConnection(
+                "jdbc:postgresql://ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/d82q285u8akq13",
+                "pjmqbdledqjucs",
+                "030c8df346d06432b9a5a4ed0bf42e56dc34761fda8d3cc04af8085dfb4f7c2b");
+    }
+
+
     // Singleton
     private final static ProductionHandler prHandler = new ProductionHandler("productionData");
 
