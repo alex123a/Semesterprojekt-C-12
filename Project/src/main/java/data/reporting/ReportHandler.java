ccc5a678192501baafb71653b2f194ca6afedfbd
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2021-05-18 16:13:30 +0200

    Begyndt på reporting

---
+++ Project/src/main/java/data/reporting/ReportHandler.java
@@ -0,0 +1,30 @@
+package data.reporting;
+
+import Interfaces.IReporting;
+
+public class ReportHandler implements IReporting {
+    @Override
+    public int getTotalCreditCount() {
+        return 0;
+    }
+
+    @Override
+    public void generateProducerCreditsCount() {
+
+    }
+
+    @Override
+    public void generateCreditTypeCount() {
+
+    }
+
+    @Override
+    public void generate10MostCredited() {
+
+    }
+
+    @Override
+    public void generateCreditsReport() {
+
+    }
+}
