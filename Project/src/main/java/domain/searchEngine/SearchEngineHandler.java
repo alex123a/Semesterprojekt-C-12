b6199ebb169de0cb8bc44276edfdf35ea50f6d24
Thomas <Raithdk@gmail.com>
2021-05-18 11:50:26 +0200

    search engine update

--- Project/src/main/java/domain/searchEngine/SearchEngineHandler.java
+++ Project/src/main/java/domain/searchEngine/SearchEngineHandler.java
@@ -8,46 +8,76 @@ import enumerations.ProductionSorting;
 import enumerations.ProductionType;
 import enumerations.RightholderSorting;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class SearchEngineHandler implements Interfaces.ISearchCredits {
     @Override
-    public List<?> findMatch(List<ISearchable> list, String target) {
+    public List<ISearchable> findMatch(List<ISearchable> list, String target) {
+
+        List<ISearchable> results = new ArrayList<>();
+        for (ISearchable ob : list){
+            if (ob.getName().contains(target)){
+                results.add(ob);
+            }
+        }
         //finder en liste af ting der overholder target regex %target%
-        return null;
+        return results;
     }
 
     @Override
     public List<IRightsholder> sortPersonBy(List<IRightsholder> list, RightholderSorting type) {
-
-
-
-        //if(type == NAME){
-        list.sort(new Comparator<IRightsholder>() {
-            @Override
-            public int compare(IRightsholder o1, IRightsholder o2) {
-                return o2.getName().compareTo(o2.getName());
-            }
-        });
-        return list;
-        //}if(type == LASTNAME){
-        list.sort(new Comparator<IRightsholder>() {
-            @Override
-            public int compare(IRightsholder o1, IRightsholder o2) {
-                return o2.getLastName().compareTo(o2.getName());
-            }
-        });
-        //}
-
         //navn og efternavn
+
+        if(type == RightholderSorting.FIRST_NAME){
+        list.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return list;
+        }
+
+        if(type == RightholderSorting.FIRST_NAME_REVERSE){
+            list.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
+            return list;
+        }
+
+        if(type == RightholderSorting.LAST_NAME){
+        //list.sort((o1, o2) -> o1.getLastName().compareTo(o2.getLastName()));
+            return list;
+        }
+//todo rightsholder get lastname
+        if(type == RightholderSorting.LAST_NAME_REVERSE){
+          //  list.sort((o1, o2) -> o2.getLastName().compareTo(o1.getLastName()));
+            return list;
+        }
+
         return null;
     }
 
     @Override
     public List<IProduction> sortProductionBy(List<IProduction> list, ProductionSorting target) {
         //navn og årstal
+        if(target == ProductionSorting.NAME){
+            list.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+            return list;
+        }
+
+        if(target == ProductionSorting.NAME_REVERSE){
+            list.sort((o1, o2) -> o2.getName().compareTo(o1.getName()));
+            return list;
+
+        }
+//todo getyear
+        if(target == ProductionSorting.YEAR){
+           // list.sort((o1, o2) -> o2.getYear().compareTo(o1.getName()));
+            return list;
+
+        }
+
+        if(target == ProductionSorting.YEAR_REVERSE){
+//todo implement reverse sort
+        }
+
         return null;
     }
 
@@ -56,15 +86,21 @@ public class SearchEngineHandler implements Interfaces.ISearchCredits {
         //årstal, genre og type
         if(yearInterval != null){
             for (IProduction prod : list){
-                int year = 9999; //prod.getYear();
+                int year = 9999; //prod.getYear(); //todo get actual year
                 if (year < yearInterval[0] || year > yearInterval[1]){
                     list.remove(prod);
                 }
             }
         }
+
+        //todo implement when prod is implemented
         if(genre != null){
-            list.removeIf(prod -> prod.getGenre != genre);
+        //    list.removeIf(prod -> prod.getGenre != genre);
         }
-        return null;
+        if(type != null){
+        //    list.removeIf(prod -> prod.getType != type);
+        }
+
+        return list;
     }
 }
