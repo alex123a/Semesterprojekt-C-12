96923de3cdf395a2ef34b44dada3995d5987886c
JanikSunke <janik.sunke@hotmail.dk>
2021-05-18 09:20:46 +0200

    facade rettelse

--- Project/src/main/java/Interfaces/IPersistenceFacade.java
+++ Project/src/main/java/Interfaces/IPersistenceFacade.java
@@ -2,17 +2,5 @@ package Interfaces;
 
 import java.util.List;
 
-public interface IPersistenceFacade {
-
-    List<IUser> getUsers();
-
-    boolean makeUserProducer(IUser user);
-
-    boolean makeUserAdmin(IUser user);
-
-    boolean deleteUser(IUser user);
-
-    boolean editUser(IUser user);
-
-    String getDatabasePassword(IUser user);
+public interface IPersistenceFacade extends IReporting, IUserFacade, IFacadeData, INotificationHandler, INotificationProvider {
 }
--- Project/src/main/java/data/PersistenceFacade.java
+++ Project/src/main/java/data/PersistenceFacade.java
@@ -1,7 +1,6 @@
 package data;
 
-import Interfaces.IPersistenceFacade;
-import Interfaces.IUser;
+import Interfaces.*;
 import data.userHandling.UserFacade;
 
 import java.util.List;
@@ -41,5 +40,90 @@ public class PersistenceFacade implements IPersistenceFacade {
 
     public static PersistenceFacade getInstance() {
         return PERSISTENCE_FACADE;
+    }
+
+    @Override
+    public IRightsholder getRightsholder(int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<IRightsholder> getRightsholders() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void insertRightsholder(IRightsholder rightsholder) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IProduction getProduction(String id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<IProduction> getProductions() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void insertProduction(IProduction production) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteProduction(IProduction production) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean createProducerNotification(int userId, INotification notification) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean createAdminNotification(INotification notification) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean deleteNotification(int notificationId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean editNotification(INotification newNotification) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<INotification> getNotifications(int userId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getTotalCreditCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void generateProducerCreditsCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void generateCreditTypeCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void generate10MostCredited() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void generateCreditsReport() {
+        throw new UnsupportedOperationException();
     }
 }
