ceb18a06e8944b010cd489f798975bdf02926c32
Mikkel <mikkelpriisholm@gmail.com>
2021-05-19 11:41:02 +0200

    Added new Better Menu
    This wont be used for now, only if it gets finished on time

---
+++ Project/src/main/java/presentation/controllers/BetterController.java
@@ -0,0 +1,300 @@
+package presentation.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import presentation.Repository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class BetterController implements Initializable {
+
+    public Label tv2Default;
+    public Label tv2Sport;
+    public Label tv2Charlie;
+    public Label tv2SportX;
+    public Label tv2News;
+    public Label tv2Zulu;
+    public Label tv2Fri;
+    public Label tv2DefaultTime;
+    public Label tv2SportTime;
+    public Label tv2SportXTime;
+    public Label tv2CharlieTime;
+    public Label tv2NewsTime;
+    public Label tv2ZuluTime;
+    public Label tv2FriTime;
+    public Label subject;
+    @FXML
+    private Button menuMyBroadcast;
+    @FXML
+    private Button menuHelp;
+    @FXML
+    private Button menuHomepage;
+    @FXML
+    private Button menuEditUser;
+    @FXML
+    private VBox defaultBox;
+    @FXML
+    private VBox sportBox;
+    @FXML
+    private Label sportProgramName;
+    @FXML
+    private Label sportProgramTime;
+    @FXML
+    private VBox sportXBox;
+    @FXML
+    private Label sportXProgramName;
+    @FXML
+    private Label sportXProgramTime;
+    @FXML
+    private VBox charlieBox;
+    @FXML
+    private Label charlieProgramName;
+    @FXML
+    private Label charlieProgramTIme;
+    @FXML
+    private VBox newBox;
+    @FXML
+    private Label newsProgramName;
+    @FXML
+    private Label newsProgramTime;
+    @FXML
+    private VBox zuluBox;
+    @FXML
+    private Label zuluProgramName;
+    @FXML
+    private Label zuluProgramTime;
+    @FXML
+    private VBox friBox;
+    @FXML
+    private Label friProgramName;
+    @FXML
+    private Label friProgramTime;
+    @FXML
+    private Label loginBtn;
+    @FXML
+    private ImageView accountImage;
+    @FXML
+    private ImageView searchImage;
+    @FXML
+    private VBox testBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM");
+        subject.setText("TV-GUIDE " + formatter.format(new Date()));
+
+        List<String> strings = new ArrayList<>();
+        try {
+            URL url = new URL("https://tv.tv2.dk/");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.contains("data-start")) {
+                    strings.add(line);
+                }
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        long start;
+        long end;
+        Long time = System.currentTimeMillis() / 1000;
+        Label[] programs = {tv2Default,tv2Sport,tv2SportX,tv2Charlie,tv2News,tv2Zulu,tv2Fri};
+        Label[] times = {tv2DefaultTime,tv2SportTime,tv2SportXTime,tv2CharlieTime,tv2NewsTime,tv2ZuluTime,tv2FriTime};
+        int count = 0;
+        for (String s : strings) {
+            start = Long.parseLong(s.substring(s.lastIndexOf("data-start") + 12, s.lastIndexOf("data-stop") - 2));
+            end = Long.parseLong(s.substring(s.lastIndexOf("data-stop") + 11, s.lastIndexOf("style") - 2));
+            System.out.println("time: " + time + ", start: " + start + ", end: " + end);
+            System.out.println("Time: " + ((time/100)/60)/60);
+            if (time > start && time < end) {
+                formatter = new SimpleDateFormat("HH:mm");
+                Date startDate = new Date(start*1000);
+                Date endDate = new Date(end*1000);
+
+                VBox programBox = new VBox();
+                programBox.setPrefHeight(60);
+                Label programTitle = new Label(s.substring(s.lastIndexOf("title=\"") + 7, s.lastIndexOf("data-program-id") - 2));
+                programTitle.setStyle("-fx-font-family: 'Calibri'; -fx-font-size: 22px; -fx-font-weight: bold;");
+                Label programTime = new Label(formatter.format(startDate) + " - " + formatter.format(endDate));
+                programTime.setStyle("-fx-font-family: 'Calibri light'; -fx-font-size: 18px;");
+                programBox.getChildren().addAll(programTitle, programTime);
+
+                Separator separator = new Separator();
+                separator.setPrefWidth(200);
+
+                testBox.getChildren().addAll(programBox, separator);
+
+                //programs[count].setText(s.substring(s.lastIndexOf("title=\"") + 7, s.lastIndexOf("data-program-id") - 2));
+                //times[count].setText(formatter.format(startDate) + " - " + formatter.format(endDate));
+                count++;
+            }
+        }
+
+
+    }
+
+    @FXML
+    void onBroadcastClicked(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layout/my_productions.fxml"));
+            Stage window = Repository.getInstance().getWindow();
+            window.setScene(new Scene(root, window.getWidth(), window.getHeight()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Broadcast");
+    }
+
+    @FXML
+    void onHelpClicked(MouseEvent event) {
+        //todo onHelp
+        System.out.println("Help");
+    }
+
+    @FXML
+    void onHomepageClicked(MouseEvent event) {
+        try {
+            java.awt.Desktop.getDesktop().browse(new URI("https://tv2.dk/"));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void onEditUserClicked(MouseEvent event) {
+        //todo onEditUser
+        System.out.println("Edit User");
+
+    }
+
+    @FXML
+    void onSearchClicked() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layout/search.fxml"));
+            Stage window = (Stage) menuMyBroadcast.getScene().getWindow();
+            window.setScene(new Scene(root, 1300, 700));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @FXML
+    void onLoginClicked(MouseEvent event) {
+        //todo onLogin
+
+    }
+
+    @FXML
+    void onDefaultBoxClicked(MouseEvent event) {
+        //todo implement default box
+        System.out.println("Default");
+    }
+
+    @FXML
+    void onSportBoxClicked(MouseEvent event) {
+        //todo implement sport box
+        System.out.println("Sport");
+    }
+
+    @FXML
+    void onSportXBoxClicked(MouseEvent event) {
+        //todo implement sport X box
+        System.out.println("Sport X");
+    }
+
+    @FXML
+    void onCharlieBoxClicked(MouseEvent event) {
+        //todo implement Charlie box
+        System.out.println("Charlie");
+    }
+
+    @FXML
+    void onNewsBoxClicked(MouseEvent event) {
+        //todo implement news box
+        System.out.println("News");
+    }
+
+    @FXML
+    void onZuluBoxClicked(MouseEvent event) {
+        //todo implement Zulu box
+        System.out.println("Zulu");
+    }
+
+    @FXML
+    void onFriBoxClicked(MouseEvent event) {
+        //todo implement fri box
+        System.out.println("Fri");
+    }
+
+    public void onMouseEnteredAccount(MouseEvent mouseEvent) {
+        Image image = new Image(getClass().getResourceAsStream("/images/Account_Red.jpg"));
+        accountImage.setImage(image);
+    }
+
+    public void onMouseExitedAccount(MouseEvent mouseEvent) {
+        Image image = new Image(getClass().getResourceAsStream("/images/Account_Grey.jpg"));
+        accountImage.setImage(image);
+    }
+
+    public void onMouseEnteredSearch(MouseEvent mouseEvent) {
+        Image image = new Image(getClass().getResourceAsStream("/images/Search_Red.jpg"));
+        searchImage.setImage(image);
+    }
+
+    public void onMouseExitedSearch(MouseEvent mouseEvent) {
+        Image image = new Image(getClass().getResourceAsStream("/images/Search_Grey.jpg"));
+        searchImage.setImage(image);
+    }
+
+
+    public void goToNotifications(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layout/notification.fxml"));
+            Stage window = (Stage) menuMyBroadcast.getScene().getWindow();
+            window.setScene(new Scene(root, 1300, 700));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void goToLogin(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layout/login.fxml"));
+            Stage window = (Stage) menuMyBroadcast.getScene().getWindow();
+            window.setScene(new Scene(root, 1300, 700));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
