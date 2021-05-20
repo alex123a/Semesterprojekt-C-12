7cb27e90034d807cea866e3e26a0daa69a591eac
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2021-05-20 10:57:13 +0200

    Removed unused code

--- Project/src/main/java/presentation/controllers/ManageUserController.java
+++ Project/src/main/java/presentation/controllers/ManageUserController.java
@@ -78,56 +78,18 @@ public class ManageUserController {
         IUser currentUser = DomainFacade.getInstance().getCurrentUser();
         if (DomainFacade.getInstance().validateUser(currentUser)) {
             String searchedUser = editSearchUsername.getText();
-            System.out.println("Search : " + searchedUser);
+            //Temporary user to fetch the wanted user from the DB.
             IUser tempUser = new User(searchedUser);
             IUser user = DomainFacade.getInstance().getUser(tempUser);
-            if (!changeUsername.getText().equals("") || !changePassword.getText().equals("")) {
-                if (!changeUsername.getText().equals("")) {
-                    System.out.println("username: " + changeUsername.getText());
-                    user.setUsername(changeUsername.getText());
-                }
-                if (!changePassword.getText().equals("")) {
-                    user.setPassword(DomainFacade.getInstance().generateStrongPasswordHash(changePassword.getText()));
-                }
-                DomainFacade.getInstance().editUser(user);
+
+            if (!changeUsername.getText().equals("")) {
+                user.setUsername(changeUsername.getText());
             }
+            if (!changePassword.getText().equals("")) {
+                user.setPassword(DomainFacade.getInstance().generateStrongPasswordHash(changePassword.getText()));
+            }
+            DomainFacade.getInstance().editUser(user);
         }
-    }
-
-    private boolean createUser(String username, String password, String type) {
-        IUser currentUser = DomainFacade.getInstance().getCurrentUser();
-        if(DomainFacade.getInstance().validateUser(currentUser)) {
-            IUser user = (type.equals("Systemadministrator")) ? new Systemadministrator(username, password) : new Producer(username, password);
-            return DomainFacade.getInstance().addUser(user);
-        }
-        return false;
-    }
-
-    private IUser createTempUser(String username) {
-        return new User(username);
-    }
-
-    private boolean changeUsername(String userUsername, String newUsername) {
-        IUser currentUser = DomainFacade.getInstance().getCurrentUser();
-        if (DomainFacade.getInstance().validateUser(currentUser)) {
-            IUser tempUser = createTempUser(userUsername);
-            IUser user = DomainFacade.getInstance().getUser(tempUser);
-            user.setUsername(newUsername);
-            return DomainFacade.getInstance().editUser(user);
-        }
-        return false;
-    }
-
-    private boolean changePassword(String userUsername, String newPassword) {
-        IUser currentUser = DomainFacade.getInstance().getCurrentUser();
-        if(DomainFacade.getInstance().validateUser(currentUser))
-        {
-            IUser tempUser = createTempUser(userUsername);
-            IUser user = DomainFacade.getInstance().getUser(tempUser);
-            user.setPassword(newPassword);
-            return DomainFacade.getInstance().editUser(user);
-        }
-        return false;
     }
 
     @FXML
