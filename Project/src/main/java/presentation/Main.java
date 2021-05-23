fa05294ef76fe9269f1529fa81662f4ab8f14a31
JanikSunke <janik.sunke@hotmail.dk>
2021-05-23 18:49:14 +0200

    Der kan trykkes på forsiden, og så lander teksten inde i søgningen

--- Project/src/main/java/presentation/Main.java
+++ Project/src/main/java/presentation/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +15,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/layout/menu.fxml"));
 
         Repository.getInstance().setWindow(primaryStage);
-        primaryStage.setTitle("TV2 Krediteringssystem");
+        primaryStage.setTitle(" Krediteringssystem");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/Tv2Logo.png")));
         primaryStage.setScene(new Scene(root, 1300, 700));
         primaryStage.show();
     }
