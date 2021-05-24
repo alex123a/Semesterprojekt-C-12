cb6bd41d173aae09589f2db4c8d876d64a5dd37d
Mikkel <mikkelpriisholm@gmail.com>
2021-05-24 15:24:24 +0200

    Revert "Fixed Go Back"
    This reverts commit 561d4dc9038acc302893d6d8132fc982b4599be8.

--- Project/src/main/java/presentation/controllers/NotificationController.java
+++ Project/src/main/java/presentation/controllers/NotificationController.java
@@ -128,6 +128,7 @@ public class NotificationController implements Initializable {
             hbox.setOnMouseClicked(new EventHandler() {
                 @Override
                 public void handle(Event event) {
+                    // TODO : find the production from the productionID and set it to setToEdit
                     Repository.getInstance().setToEdit(production);
 
                     try {
